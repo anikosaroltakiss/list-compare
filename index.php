61f7fd7b99f6ce9495ff2f7852fc874166817a9c
anikosaroltakiss <anikosaroltakiss@gmail.com>
2026-04-07 15:27:52 +0200

    Twig hozzáadása

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use ListCompare\Infrastructure\TemplateSystem\Renderer;
+
+$renderer = new Renderer(__DIR__ . '/templates');
+
+echo $renderer->render('layout.html.twig');
---
+++ src/Infrastructure/TemplateSystem/Renderer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ListCompare\Infrastructure\TemplateSystem;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+class Renderer
+{
+
+    private Environment $twig;
+
+    /**
+     * @param string $templatePath
+     */
+    public function __construct(string $templatePath)
+    {
+        $loader = new FilesystemLoader($templatePath);
+
+        $this->twig = new Environment($loader, [
+            'debug' => true,
+            'cache' => false,
+            'strict_variables' => true
+        ]);
+    }
+
+    /**
+     * @param string $path
+     * @param array $vars
+     * @return string
+     */
+    public function render(string $name, array $vars = []): string
+    {
+        return $this->twig->render($name, $vars);
+    }
+}

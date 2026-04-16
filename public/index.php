8981a0502a1eb4aeb5ff188cf32369ec97b22b79
anikosaroltakiss <anikosaroltakiss@gmail.com>
2026-04-16 21:50:55 +0200

    public mappa hozzáadása

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use ListCompare\Infrastructure\Http\Router;
 use ListCompare\Infrastructure\TemplateSystem\Renderer;
 
-$renderer = new Renderer(__DIR__ . '/templates');
+$renderer = new Renderer(__DIR__ . '/../templates');
 $router = new Router($renderer);
 
 $router->handleRequest($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
